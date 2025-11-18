c11fdd5f2146d93b7a69e5352a0fdc359357ee56
Ezra Bimantara <bimantaraezra@gmail.com>
2025-11-18 14:22:50 +0700

    nambah ml model lho ya

--- workpool.py
+++ workpool.py
@@ -3,7 +3,7 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source = "https://github.com/EzraBimantara/Prefect.git",
-        entrypoint="103task.py:pipeline",
+        entrypoint="machinelearning.py:pipeline",
     ).deploy(
         name="lab-103-deploy",
         work_pool_name="Workpool",
